673d01f2bf9f7659e4fa8bfae37116f3842f6364
Kartik Raj <120095899+Krrtik1122@users.noreply.github.com>
2025-11-16 18:29:57 +0530

    Add files via upload

---
+++ inventory-service/src/main/java/com/krrtk/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.krrtk.inventoryservice.repository;
+
+import com.krrtk.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
